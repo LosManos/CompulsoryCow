5e1951b52e28e1a20a1704be4ae9d49776509f6a
Ola Fjelddahl <ola.fjelddahl(diespammers)@gmail.com>
2012-09-29 23:20:23 +0200

    Intermediary checkin, 4 tests are running.

--- CompulsoryCow/StringExtension/StringExtension.cs
+++ CompulsoryCow/StringExtension/StringExtension.cs
@@ -2,39 +2,117 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CompulsoryCow.StringExtension
 {
-    public static class StringExtension
-    {
-        public static string SFormat(this string format, params object[] parts)
-        {
-            try
-            {
-                return SafeFormat(format, parts);
-            }
-            catch (Exception exc)
-            {
-                return FormatFormatstringAndParameters(format, parts);
-            }
-        }
+	public static class StringExtension
+	{
+		private const string FailedFormatting = "Failed formatting.";
+		private const string ParametersAre = "Parameters are:";
+		private const string ParametersMissing = "Parameter(s) missing.";
 
-        private static string FormatFormatstringAndParameters(string format, object[] parts)
-        {
-            throw new NotImplementedException();
-        }
+		public static string SFormat(this string format, params object[] parts)
+		{
+			//  Since we know that the SafeFormat handles _all_ exceptions we don't have to try-catch here.
+			return SafeFormat(format, parts);
+		}
 
-        public static string SafeFormat(string format, params object[] parts)
-        {
-            try
-            {
-                return string.Format(format, parts);
-            }
-            catch (Exception exc)
-            {
-                return FormatFormatstringAndParameters(format, parts);
-            }
-        }
-    }
+		public static string SafeFormat(string format, params object[] parts)
+		{
+			try
+			{
+				try
+				{
+					return string.Format(format, parts);
+				}
+				catch (FormatException exc)
+				{
+					return FormatFormatstringAndParameters(format, parts, exc);
+				}
+				catch (ArgumentNullException exc)
+				{
+					return FormatFormatstringAndParameters(format, parts, exc);
+				}
+			}
+			catch (Exception innerExc)
+			{
+				return innerExc.Message;
+			}
+		}
+
+		private static string FormatFormatstringAndParameters(string format, object[] parts, Exception exc)
+		{
+			//  We've got an error.  Find out which type of error and format accordingly.
+
+			if (IsArgumentElementMissing(format, parts))
+			{
+				return StringOrEmpty(format) + "[" + FailedFormatting + " " + ParametersMissing +
+					(IsPartsMissing(parts) ? string.Empty : " " + FormatParameters(parts)) +
+					"]";
+			}
+
+			if (IsFormatMissing(format) && IsPartsMissing( parts ))
+			{
+				//	Ok.
+				return string.Empty;
+			}
+
+			throw new NotImplementedException("This method does still not handle...");
+		}
+
+		private static bool IsPartsMissing(object[] parts)
+		{
+			return null == parts || parts.Length == 0;
+		}
+
+		private static bool IsFormatMissing(string format)
+		{
+			return null == format;
+		}
+
+		private static string FormatParameters(object[] parts)
+		{
+			var lst = new List<string>();
+			if (null != parts)
+			{
+				foreach (var part in parts)
+				{
+					lst.Add(part.GetType().ToString() + "," +
+						(part.GetType() == typeof(string) ? "'" + part.ToString() + "'" : part.ToString())
+						);
+				}
+			}
+			return "{" + string.Join(",", lst) + "}";
+		}
+
+		private static string StringOrEmpty(string arg)
+		{
+			return null == arg ? string.Empty : arg;
+		}
+
+		private static bool IsArgumentElementMissing(string format, object[] parts)
+		{
+			return FormatElementCount(format) > PartsLength(parts);
+		}
+
+		private static int FormatElementCount(string format)
+		{
+			return Regex.Matches(StringOrEmpty(format), @"\{[0-9]+\}").Count;
+		}
+
+		private static int PartsLength(object[] parts)
+		{
+			if (null == parts)
+			{
+				return 0;
+			}
+			else
+			{
+				return parts.Length;
+			}
+		}
+
+	}
 }
--- StringExtensionTest/UnitTest1.cs
+++ StringExtensionTest/UnitTest1.cs
@@ -15,17 +15,31 @@ namespace StringExtensionTest
         }
 
         [TestMethod]
-        public void NoArgument()
+        public void TooFewArgumentsNoArgument()
         {
             var res = "a{0}c".SFormat();
-            Assert.AreEqual("a{0}c[Failed formatting.  Parameter(s) missing in format string.", res);
+            Assert.AreEqual("a{0}c[Failed formatting. Parameter(s) missing.]", res);
+		}
+
+		[TestMethod]
+		public void TooFewArguments()
+		{
+			var res = "a{0}c{1}e".SFormat("b");
+			Assert.AreEqual("a{b}c{1}e[Failed formatting. Parameter(s) missing. Parameter(s) is/are:{System.String,'b'}.]", res);
+		}
+
+        [TestMethod]
+        public void NoNothing()
+        {
+            var res = ((string)null).SFormat();
+            Assert.AreEqual(string.Empty, res, "It should always returns a string, empty is so.");
         }
 
         [TestMethod]
         public void OneTooManyArguments()
         {
             var res = "a{0}c".SFormat("b", "d");
-            Assert.AreEqual("abc[Failed formatting parameter(s) ['d'] was/were not used in format string.", res);
+            Assert.AreEqual("abc[Failed formatting parameter(s) ['d'] was/were not used in format string.]", res);
         }
 
         [TestMethod]
@@ -42,10 +56,10 @@ namespace StringExtensionTest
             Assert.AreEqual(string.Empty, res, "SafeFormat should alwas return a string.");
 
             res = ((string)null).SFormat("a");
-            Assert.AreEqual("[Failed formatting.  Format string was empty. Parameter(s) ['a'] was/were not used in format string.]", res);
+            Assert.AreEqual("[Failed formatting. Format string was empty. Parameter(s) ['a'] was/were not used in format string.]", res);
             
             res = ((string)null).SFormat("a", "b");
-            Assert.AreEqual("[Failed formatting.  Format string was empty. Parameter(s) ['a','b'] was/were not used in format string.]", res);
+            Assert.AreEqual("[Failed formatting. Format string was empty. Parameter(s) ['a','b'] was/were not used in format string.]", res);
         }
 
         /// <summary>This method just makes sure there is a static method.
@@ -65,7 +79,7 @@ namespace StringExtensionTest
         {
             //  Using this exact combination will throw an internal exception but it is caught and nicer output is outputed.
             var res = "a{0}c".SFormat( Guid.Parse( "{E981C86F-C8D5-46AC-84FB-1AB281A2390B}"));
-            Assert.AreEqual("a{0}c,[Failed formatting.  Parameter(s) is/are ['System.Guid:{E981C86F-C8D5-46AC-84FB-1AB281A2390B}']  Exception message is: Internal exception with message ", res);
+            Assert.AreEqual("a{0}c,[Failed formatting. Parameter(s) is/are ['System.Guid:{E981C86F-C8D5-46AC-84FB-1AB281A2390B}']  Exception message is: Internal exception with message ", res);
 
             //  Using this exact combination will throw and exception when handling the exception and the code outputs the exception message.
             res = "a{0}c".SFormat( Guid.Parse("{4543AF85-D1A4-4EA3-81AF-EEE2FE7C766F}"));
@@ -80,12 +94,24 @@ namespace StringExtensionTest
         {
             //  Using this exact combination will throw an internal exception but it is caught and nicer output is outputed.
             var res = CompulsoryCow.StringExtension.StringExtension.SafeFormat("a{0}c",Guid.Parse("{E981C86F-C8D5-46AC-84FB-1AB281A2390B}"));
-            Assert.AreEqual("a{0}c,[Failed formatting.  Parameter(s) is/are ['System.Guid:{E981C86F-C8D5-46AC-84FB-1AB281A2390B}']  Exception message is: Internal exception with message ", res);
+            Assert.AreEqual("a{0}c,[Failed formatting. Parameter(s) is/are ['System.Guid:{E981C86F-C8D5-46AC-84FB-1AB281A2390B}']  Exception message is: Internal exception with message ", res);
 
             //  Using this exact combination will throw and exception when handling the exception and the code outputs the exception message.
             res = CompulsoryCow.StringExtension.StringExtension.SafeFormat("a{0}c",Guid.Parse("{4543AF85-D1A4-4EA3-81AF-EEE2FE7C766F}"));
             Assert.AreEqual("The error should be visible here", res);
         }
 
+		[TestMethod]
+		public void FormatStringParser()
+		{
+			//	How do we get inside to internal functions without making teh method public. Protected?  Special method attribute?  Reflection?  Obsolete flag?
+			Assert.Fail("Create a thorough test for how the format string is parsed and understood.");
+		}
+
+		[TestMethod]
+		public void UnhandledFormatFormatstringAndParameters()
+		{
+			Assert.Fail("How do we make test for this?");
+		}
     }
 }
