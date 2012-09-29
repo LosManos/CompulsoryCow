5929be08289e4198e2f1ef1f527f734edbf1bf97
Ola Fjelddahl <ola.fjelddahl(diespammers)@gmail.com>
2012-09-29 22:04:13 +0200

    All tests I could think of are written.
    I will most certainly come up with more as time goes by.

---
+++ CompulsoryCow/StringExtension/StringExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompulsoryCow.StringExtension
+{
+    public static class StringExtension
+    {
+        public static string SFormat(this string format, params object[] parts)
+        {
+            try
+            {
+                return SafeFormat(format, parts);
+            }
+            catch (Exception exc)
+            {
+                return FormatFormatstringAndParameters(format, parts);
+            }
+        }
+
+        private static string FormatFormatstringAndParameters(string format, object[] parts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string SafeFormat(string format, params object[] parts)
+        {
+            try
+            {
+                return string.Format(format, parts);
+            }
+            catch (Exception exc)
+            {
+                return FormatFormatstringAndParameters(format, parts);
+            }
+        }
+    }
+}
---
+++ StringExtensionTest/UnitTest1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CompulsoryCow.StringExtension;
+
+namespace StringExtensionTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NormalSucceed()
+        {
+            var res = "a{0}c".SFormat("b");
+            Assert.AreEqual("abc", res);
+        }
+
+        [TestMethod]
+        public void NoArgument()
+        {
+            var res = "a{0}c".SFormat();
+            Assert.AreEqual("a{0}c[Failed formatting.  Parameter(s) missing in format string.", res);
+        }
+
+        [TestMethod]
+        public void OneTooManyArguments()
+        {
+            var res = "a{0}c".SFormat("b", "d");
+            Assert.AreEqual("abc[Failed formatting parameter(s) ['d'] was/were not used in format string.", res);
+        }
+
+        [TestMethod]
+        public void TwoTooManyArguments()
+        {
+            var res = "a{0}c".SFormat("b", "d", "e");
+            Assert.AreEqual("abc[Failed formatting parameter(s) ['d','e'] was/were not used in format string.]", res);
+        }
+
+        [TestMethod]
+        public void NullFormatVariousParameters()
+        {
+            var res = ((string)null).SFormat();
+            Assert.AreEqual(string.Empty, res, "SafeFormat should alwas return a string.");
+
+            res = ((string)null).SFormat("a");
+            Assert.AreEqual("[Failed formatting.  Format string was empty. Parameter(s) ['a'] was/were not used in format string.]", res);
+            
+            res = ((string)null).SFormat("a", "b");
+            Assert.AreEqual("[Failed formatting.  Format string was empty. Parameter(s) ['a','b'] was/were not used in format string.]", res);
+        }
+
+        /// <summary>This method just makes sure there is a static method.
+        /// </summary>
+        [TestMethod]
+        public void StaticMethod()
+        {
+            var res = CompulsoryCow.StringExtension.StringExtension.SafeFormat("a{0}c", "b");
+            Assert.AreEqual("abc", res);
+        }
+
+        /// <summary>This method, in debug compile, throws an exception.
+        /// Which gets swallowed and a string is returned.
+        /// </summary>
+        [TestMethod]
+        public void ForcedException()
+        {
+            //  Using this exact combination will throw an internal exception but it is caught and nicer output is outputed.
+            var res = "a{0}c".SFormat( Guid.Parse( "{E981C86F-C8D5-46AC-84FB-1AB281A2390B}"));
+            Assert.AreEqual("a{0}c,[Failed formatting.  Parameter(s) is/are ['System.Guid:{E981C86F-C8D5-46AC-84FB-1AB281A2390B}']  Exception message is: Internal exception with message ", res);
+
+            //  Using this exact combination will throw and exception when handling the exception and the code outputs the exception message.
+            res = "a{0}c".SFormat( Guid.Parse("{4543AF85-D1A4-4EA3-81AF-EEE2FE7C766F}"));
+            Assert.AreEqual("The error should be visible here", res);
+        }
+
+        /// <summary>This method, in debug compile, throws an exception.
+        /// Which gets swallowed and a string is returned.
+        /// </summary>
+        [TestMethod]
+        public void StaticForcedException()
+        {
+            //  Using this exact combination will throw an internal exception but it is caught and nicer output is outputed.
+            var res = CompulsoryCow.StringExtension.StringExtension.SafeFormat("a{0}c",Guid.Parse("{E981C86F-C8D5-46AC-84FB-1AB281A2390B}"));
+            Assert.AreEqual("a{0}c,[Failed formatting.  Parameter(s) is/are ['System.Guid:{E981C86F-C8D5-46AC-84FB-1AB281A2390B}']  Exception message is: Internal exception with message ", res);
+
+            //  Using this exact combination will throw and exception when handling the exception and the code outputs the exception message.
+            res = CompulsoryCow.StringExtension.StringExtension.SafeFormat("a{0}c",Guid.Parse("{4543AF85-D1A4-4EA3-81AF-EEE2FE7C766F}"));
+            Assert.AreEqual("The error should be visible here", res);
+        }
+
+    }
+}
