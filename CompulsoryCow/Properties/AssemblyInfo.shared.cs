a40322d4d5aea351fa13458da94879dc51daaf5c
LosManos <ola.fjelddahl(diespammers)@gmail.com>
2013-04-04 22:10:36 +0200

    Updates for making resharper happy.

--- CompulsoryCow/Properties/AssemblyInfo.shared.cs
+++ CompulsoryCow/Properties/AssemblyInfo.shared.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
+// ReSharper disable RedundantUsingDirective
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+// ReSharper restore RedundantUsingDirective
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
