d8b0e3edce58de14d5662e02c9be6f5ca2b2da49
ola.fjelddahl@(diespammers)gmail.com <ola.fjelddahl@.diespammers.gmail.com>
2018-05-20 23:20:19 +0200

    Nuspec updated to 2.4.2.

--- BuildNugetPackage.ps1
+++ BuildNugetPackage.ps1
@@ -1,5 +1,8 @@
 Write-Host 'Packing the nuget package.'
 
+Write-Host 'Make sure the solution has been built as Relase first.'
+Read-Host 'Press return when ready'
+
 pushd CompulsoryCow
 
 .\..\nuget.exe pack -Prop Configuration=Release
