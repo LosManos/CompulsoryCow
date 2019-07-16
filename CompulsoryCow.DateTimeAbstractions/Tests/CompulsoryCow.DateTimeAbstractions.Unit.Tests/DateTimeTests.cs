b6a06a45b59f7bae9bd3a20387608779768f7472
LosManos <ola.fjelddahl@(diespammers)gmail.com>
2019-07-16 23:13:15 +0000

    Use VacheTache library

--- CompulsoryCow.DateTimeAbstractions/Tests/CompulsoryCow.DateTimeAbstractions.Unit.Tests/DateTimeTests.cs
+++ CompulsoryCow.DateTimeAbstractions/Tests/CompulsoryCow.DateTimeAbstractions.Unit.Tests/DateTimeTests.cs
@@ -1,14 +1,27 @@
 using FluentAssertions;
+using System.Reflection;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace CompulsoryCow.DateTimeAbstractions.Unit.Tests
 {
-    public class UnitTest1
+    public class DateTimeTests
     {
+        private readonly VacheTacheLibrary.PseudoRandom _pr;
+
+        public DateTimeTests(ITestOutputHelper output)
+        {
+            var type = output.GetType();
+            var testMember = type.GetField("test", BindingFlags.Instance | BindingFlags.NonPublic);
+            var test = (ITest)testMember.GetValue(output);
+            _pr = new VacheTacheLibrary.PseudoRandom(test.DisplayName);
+
+        }
+
         [Fact]
         public void AddShouldAdd()
         {
-            var anyTicks = 1000;
+            var anyTicks = AnyTicks();
             var anyTimeSpan = new CompulsoryCow.DateTimeAbstractions.TimeSpan();
             var sut = new CompulsoryCow.DateTimeAbstractions.DateTime(anyTicks);
 
@@ -16,13 +29,18 @@ namespace CompulsoryCow.DateTimeAbstractions.Unit.Tests
 
             var expected = new global::System.DateTime(anyTicks)
                 .Add(anyTimeSpan.ToSystemTimeSpan());
-            Equals(expected, res);
+            AssertEquals(expected, res);
+        }
+
+        private long AnyTicks()
+        {
+            return _pr.PositiveLong(1, global::System.DateTime.MaxValue.Ticks);
         }
 
         private static void AssertEquals(
             global::System.DateTime expectedDateTime,
             DateTimeAbstractions.DateTime actualDateTime, 
-            string because)
+            string because = "")
         {
             actualDateTime.Ticks.Should().Be(expectedDateTime.Ticks, because);
             actualDateTime.Kind.Should().Be(expectedDateTime.Kind, because);
