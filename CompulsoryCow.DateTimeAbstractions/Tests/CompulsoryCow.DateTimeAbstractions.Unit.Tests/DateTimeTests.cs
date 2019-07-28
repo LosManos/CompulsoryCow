6c4e4573a83366976a917c1a9a155e52828ac09d
LosManos <ola.fjelddahl@(diespammers)gmail.com>
2019-07-28 22:52:12 +0000

    Complete Year_Month_Day_Hour_Minute_Second_Millisecond_Kind construct tests

--- CompulsoryCow.DateTimeAbstractions/Tests/CompulsoryCow.DateTimeAbstractions.Unit.Tests/DateTimeTests.cs
+++ CompulsoryCow.DateTimeAbstractions/Tests/CompulsoryCow.DateTimeAbstractions.Unit.Tests/DateTimeTests.cs
@@ -564,19 +564,96 @@ namespace CompulsoryCow.DateTimeAbstractions.Unit.Tests
 
     #region DateTime(int year, int month, int day, int hour, int minute, int second, int millisecond, DateTimeKind kind) tests.
 
-    // TODO:OF:Add test for if Kind is out of bounds. There are tests to copy.
-    [Fact]
+        [Fact]
         public void Constructor_Year_Month_Day_Hour_Minute_Second_Millisecond_Kind_ShouldCreate()
         {
             //  #   Arrange.
-            var anyDateTime = new System.DateTime(AnyTicks(), AnyKindExcept(default));
-            var expected = new System.DateTime(anyDateTime.Year, anyDateTime.Month, anyDateTime.Day, anyDateTime.Hour, anyDateTime.Minute, anyDateTime.Second, anyDateTime.Millisecond, anyDateTime.Kind);
+            var anyYear = 2019;
+            var anyMonth = 7;
+            var anyDay = 26;
+            var anyHour = 12;
+            var anyMinute = 34;
+            var anySecond = 56;
+            var anyMillisecond = 789;
+            var anyKind = System.DateTimeKind.Utc;
+            var systemDateTime = new System.DateTime(anyYear, anyMonth, anyDay, anyHour, anyMinute, anySecond, anyMillisecond, anyKind);
 
             //  #   Act.
-            var sut = new Abstractions.DateTime(expected.Year, expected.Month, expected.Day, expected.Hour, expected.Minute, expected.Second, expected.Millisecond, AnyKindExcept(default));
+            var sut = new Abstractions.DateTime(anyYear, anyMonth, anyDay, anyHour, anyMinute, anySecond, anyMillisecond, anyKind);
 
             //  #   Assert.
-            AssertEquals(expected, sut);
+            AssertEquals(systemDateTime, sut);
+        }
+
+        [Theory]
+        [InlineData(0, null, null, null, null, null, null, "Too low Year.")]
+        [InlineData(10000, null, null, null, null, null, null, "Too high Year.")]
+        [InlineData(null, 0, null, null, null, null, null, "Too low Month.")]
+        [InlineData(null, 13, null, null, null, null, null, "Too high Month.")]
+        [InlineData(null, null, 0, null, null, null, null, "Too low Day.")]
+        [InlineData(null, null, 32, null, null, null, null, "Too high Day.")]
+        [InlineData(null, null, null, -1, null, null, null, "Too low Hour.")]
+        [InlineData(null, null, null, 24, null, null, null, "Too high Hour.")]
+        [InlineData(null, null, null, null, -1, null, null, "Too low Minute.")]
+        [InlineData(null, null, null, null, 60, null, null, "Too high Minute.")]
+        [InlineData(null, null, null, null, null, -1, null, "Too low Second.")]
+        [InlineData(null, null, null, null, null, 60, null, "Too high Second.")]
+        [InlineData(null, null, null, null, null, null, -1, "Too low Second.")]
+        [InlineData(null, null, null, null, null, null, 1000, "Too high Second.")]
+        public void Constructor_Year_Month_Day_Hour_Minute_Second_Millisecond_Kind_ShouldThrowForOutOfRange(int? year, int? month, int? day, int? hour, int? minute, int? second, int? millisecond, string reason)
+        {
+            //  #   Arrange.
+            var anyValidYear = 2019;
+            var anyValidMonth = 7;
+            var anyValidDay = 26;
+            var anyValidHour = 12;
+            var anyValidMinute = 34;
+            var anyValidSecond = 56;
+            var anyValidMillisecond = 789;
+            var anyKind = System.DateTimeKind.Utc;
+            var anyCalendar = new System.Globalization.TaiwanCalendar();
+            var res = new Abstractions.DateTime(anyValidYear, anyValidMonth, anyValidDay, anyValidHour, anyValidMinute, anyValidSecond, anyValidMillisecond, anyKind);
+            res.Should().NotBeNull("Smoke test that we can create at all.");
+
+            //  #   Act.
+            var exc = Record.Exception(() =>
+            {
+                new Abstractions.DateTime(
+                    year ?? anyValidDay,
+                    month ?? anyValidMonth,
+                    day ?? anyValidDay,
+                    hour ?? anyValidHour,
+                    minute ?? anyValidMinute,
+                    second ?? anyValidSecond,
+                    millisecond ?? anyValidMillisecond,
+                    anyKind);
+            });
+
+            //  #   Assert.
+            exc.Should().BeOfType<System.ArgumentOutOfRangeException>(reason);
+        }
+
+        [Fact]
+        public void Constructor_Year_Month_Day_Hour_Minute_Second_Millisecond_Kind_ShouldThrowForInvalidDateTimeKind()
+        {
+            //  #   Arrange.
+            var anyYear = 2019;
+            var anyMonth = 7;
+            var anyDay = 26;
+            var anyHour = 12;
+            var anyMinute = 34;
+            var anySecond = 56;
+            var anyMillisecond = 789;
+            var unknownKind = (System.DateTimeKind)(((int)MaxDateTimeKind()) + 1);
+
+            //  #   Act.
+            var exc = Record.Exception(() =>
+            {
+                new Abstractions.DateTime(anyYear, anyMonth, anyDay, anyHour, anyMinute, anySecond, anyMillisecond, unknownKind);
+            });
+
+            //  #   Assert.
+            exc.Should().BeOfType<System.ArgumentException>();
         }
 
         #endregion
