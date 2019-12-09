78f7d2e69c57ae040bbfb17ba61e4d1ca4955714
LosManos <ola.fjelddahl@(diespammers)gmail.com>
2019-12-09 20:05:01 +0000

    Move ITimeSpan to a file of its own

--- CompulsoryCow.DateTimeAbstractions/CompulsoryCow.DateTimeAbstractions/TimeSpan.cs
+++ CompulsoryCow.DateTimeAbstractions/CompulsoryCow.DateTimeAbstractions/TimeSpan.cs
@@ -2,63 +2,6 @@
 
 namespace CompulsoryCow.DateTime.Abstractions
 {
-    public interface ITimeSpan
-    {
-        /// <summary>
-        /// <see cref="System.TimeSpan.TotalMilliseconds"/>
-        /// </summary>
-        double TotalMilliseconds { get; }
-
-        /// <summary>
-        /// <see cref="System.TimeSpan.TotalHours"/>
-        /// </summary>
-        double TotalHours { get; }
-
-        /// <summary>
-        /// <see cref="System.TimeSpan.TotalDays"/>
-        /// </summary>
-        double TotalDays { get; }
-
-        /// <summary>
-        /// <see cref="System.TimeSpan.Ticks"/>
-        /// </summary>
-        long Ticks { get; }
-
-        /// <summary>
-        /// <see cref="System.TimeSpan.Seconds"/>
-        /// </summary>
-        int Seconds { get; }
-
-        /// <summary>
-        /// <see cref="System.TimeSpan.Minutes"/>
-        /// </summary>
-        int Minutes { get; }
-
-        /// <summary>
-        /// <see cref="System.TimeSpan.Milliseconds"/>
-        /// </summary>
-        int Milliseconds { get; }
-
-        /// <summary>
-        /// <see cref="System.TimeSpan.Hours"/>
-        int Hours { get; }
-
-        /// <summary>
-        /// <see cref="System.TimeSpan.Days"/>
-        /// </summary>
-        int Days { get; }
-
-        /// <summary>
-        /// <see cref="System.TimeSpan.TotalMinutes"/>
-        /// </summary>
-        double TotalMinutes { get; }
-
-        /// <summary>
-        /// <see cref="System.TimeSpan.TotalSeconds"/>
-        /// </summary>
-        double TotalSeconds { get; }
-    }
-
     /// TODO:OF:Implement interfaces at https:///docs.microsoft.com/en-us/dotnet/api/system.timespan?view=netcore-2.2
     public struct TimeSpan : ITimeSpan
     {
