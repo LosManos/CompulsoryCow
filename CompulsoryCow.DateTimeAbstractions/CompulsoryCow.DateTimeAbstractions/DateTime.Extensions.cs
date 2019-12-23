91a1b07da8a87e2ea82328e38a54183fd118217c
LosManos <ola.fjelddahl@(diespammers)gmail.com>
2019-12-23 21:27:26 +0000

    Add methods for converting to and from Abstractions and System DateTime
    This commit adds extension methods ToSystemDateTime and ToAbstractionsDateTime.
    They are extension methods and not virtual to not pollute
    the IDateTime interface and DateTime class as we want to be as close to
    System.DateTime possible.
    
    This commit solves #21

---
+++ CompulsoryCow.DateTimeAbstractions/CompulsoryCow.DateTimeAbstractions/DateTime.Extensions.cs
@@ -0,0 +1,30 @@
+﻿namespace CompulsoryCow.DateTime.Abstractions
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>This method is for converting a <see cref="Abstractions.DateTime"/> 
+        /// to a <see cref="System.DateTime"/>.
+        /// </summary>
+        /// <param name="me"></param>
+        /// <returns></returns>
+        public static System.DateTime ToSystemDateTime(this DateTime me)
+        {
+            // Hopefully dotnetcore3 can make sure ` me`  is not null instead of throwing.
+            if( me == null)
+            {
+                throw new System.ArgumentNullException(nameof(me));
+            }
+            return new System.DateTime(me.Ticks, me.Kind);
+        }
+
+        /// <summary>This method is for convertng a <see cref="System.DateTime"/>
+        /// to a <see cref="Abstractions.DateTime"/>.
+        /// </summary>
+        /// <param name="me"></param>
+        /// <returns></returns>
+        public static DateTime ToAbstractionsDateTime(this System.DateTime me)
+        {
+            return new DateTime(me.Ticks, me.Kind);
+        }
+    }
+}
