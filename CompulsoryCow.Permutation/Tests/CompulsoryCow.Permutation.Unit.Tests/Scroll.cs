ab1e5cffc8a2c906a2745792a351ffb690775653
Ola Fjelddahl <ola.fjelddahl@omegapoint.se>
2020-01-13 15:51:42 +0000

    Add Permutate.Scroll method
    This commit solves #27.
    This commit updates the version to 0.2.0

---
+++ CompulsoryCow.Permutation/Tests/CompulsoryCow.Permutation.Unit.Tests/Scroll.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace CompulsoryCow.Permutation.Unit.Tests
+{
+    public class Scroll
+    {
+        [Fact]
+        public void CanScroll()
+        {
+            string value = "<script>";
+            string defaultValue = "x";
+            const int size = 5;
+
+            //  Act.
+            var res = Permutation.Scroll(value, defaultValue, size);
+
+            //  Assert.
+            var result = res.ToList();
+            result.Count().Should().Be(size);
+            result[0].Should().BeEquivalentTo(new[] { value, defaultValue, defaultValue, defaultValue, defaultValue });
+            result[0].Should().BeEquivalentTo(new[] { defaultValue, value, defaultValue, defaultValue, defaultValue });
+            result[0].Should().BeEquivalentTo(new[] { defaultValue, defaultValue, value, defaultValue, defaultValue });
+            result[0].Should().BeEquivalentTo(new[] { defaultValue, defaultValue, defaultValue, value, defaultValue });
+            result[0].Should().BeEquivalentTo(new[] { defaultValue, defaultValue, defaultValue, defaultValue, value });
+        }
+    }
+}
