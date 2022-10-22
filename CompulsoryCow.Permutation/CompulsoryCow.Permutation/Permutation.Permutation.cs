21cf1ff6a5dfb1830661d3a692bdf3c7a24a94a4
Ola Fjelddahl <xomptolf@mio.se>
2022-10-22 21:05:22 +0200

    (Permutation)Remove stray using
    This commit contains no functional change.

--- CompulsoryCow.Permutation/CompulsoryCow.Permutation/Permutation.Permutation.cs
+++ CompulsoryCow.Permutation/CompulsoryCow.Permutation/Permutation.Permutation.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using Parameters = System.Collections.Generic.IEnumerable<object>;
 
 namespace CompulsoryCow.Permutation;
