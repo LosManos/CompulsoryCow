c01bd792042af5eae9fefd472831a241d006f309
Ola Fjelddahl <xomptolf@mio.se>
2022-10-22 21:04:16 +0200

    (Permutation/Test)Example test for permutation of an enum
    This commit adds example code for how to write a test
    where an enum is automatically permutated
    `Permutate<MyEnum>()`
    instead of
    `Permutate(new[]{MyEnum.One, MyEnum.Two})`
    
    This commit is part of #55.

---
+++ CompulsoryCow.Permutation/Tests/CompulsoryCow.Permutation.Unit.Tests/ExampleAutomaticValuesTestss.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using static CompulsoryCow.Permutation.Unit.Tests.AuthorisationService;
+
+namespace CompulsoryCow.Permutation.Unit.Tests;
+
+public class ExampleAutomaticValuesTests
+{
+    public static IEnumerable<object[]> Variants()
+    {
+        return Permutation.Permutate(
+            new object[][] {
+                Permutation.AllItems<WebPage>(),
+                new object[]{false, true},
+                new object[]{false, true},
+            })
+            .Select(data =>
+            {
+                return new[] {
+                    data.First(),
+                    data.Skip(1).First(),
+                    data.Skip(2).First()
+                };
+            });
+    }
+
+    [Theory]
+    [MemberData(nameof(Variants))]
+    public void Example_usage(WebPage page, bool isLoggedOn, bool isAdmin)
+    {
+        var sut = new AuthorisationService();
+
+        //  # Act.
+        var res = sut.Authorise(page, isLoggedOn, isAdmin);
+
+        //  # Assert.
+        res.Should().Be(expectedResult(page, isLoggedOn, isAdmin));
+
+        static bool expectedResult(WebPage p, bool isL, bool isA)
+        {
+            if( p == WebPage.LandingPage)
+            {
+                return true;
+            }
+            if( p == WebPage.AdminPage && isL && isA)
+            {
+                return true;
+            }
+            if( p==WebPage.ContentPage && isL)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
