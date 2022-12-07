33728023f827a57cf0b8584c8b0bfe6644a46b8c
Ola Fjelddahl <xomptolf@mio.se>
2022-12-08 00:04:56 +0100

    AssemblyAbstractions:Update tests to dotnet6
    This commit updates the dotnet version of the tests.
    This commit changes tests to file based name space.
    
    This commit contains no functional change.

--- CompulsoryCow.AssemblyAbstractions/Tests/CompulsoryCow.AssemblyAbstractions.Unit.Tests/AssemblyLoadContext.Tests.cs
+++ CompulsoryCow.AssemblyAbstractions/Tests/CompulsoryCow.AssemblyAbstractions.Unit.Tests/AssemblyLoadContext.Tests.cs
@@ -7,94 +7,93 @@ using System.IO;
 using System.Linq;
 using Xunit;
 
-namespace CompulsoryCow.AssemblyAbstractions.Unit.Tests
+namespace CompulsoryCow.AssemblyAbstractions.Unit.Tests;
+
+/// <summary>This class contains tests of the (static) constructors
+/// in <see cref="System.Reflection.Assembly"/>
+/// which in AssemblyAbstractions is moved 
+/// from Assembly to AssemblyFactory
+/// as static constructors are impossible(?) to mock.
+/// </summary>
+public class AssemblyLoadContextTests
 {
-    /// <summary>This class contains tests of the (static) constructors
-    /// in <see cref="System.Reflection.Assembly"/>
-    /// which in AssemblyAbstractions is moved 
-    /// from Assembly to AssemblyFactory
-    /// as static constructors are impossible(?) to mock.
-    /// </summary>
-    public class AssemblyLoadContextTests
+    [Fact]
+    public void AllMethodsShouldBeMockable()
     {
-        [Fact]
-        public void AllMethodsShouldBeMockable()
+        // These methods are out of our control.
+        var objectMethods = new[]
         {
-            // These methods are out of our control.
-            var objectMethods = new[]
-            {
-                    nameof(AssemblyFactory.Equals),
-                    nameof(AssemblyFactory.GetHashCode),
-                    nameof(AssemblyFactory.GetType),
-                    nameof(AssemblyFactory.ToString),
-            };
+                nameof(AssemblyFactory.Equals),
+                nameof(AssemblyFactory.GetHashCode),
+                nameof(AssemblyFactory.GetType),
+                nameof(AssemblyFactory.ToString),
+        };
 
-            var methods = typeof(AssemblyLoadContext)
-                .GetMethods()
-                .Where(m => m.IsConstructor == false)
-                .Where(m => objectMethods.Contains(m.Name) == false
-                );
-
-            var res = methods
-                .Select(m => m.IsVirtual);
-
-            //  Act.
-            res.Count().Should().Be(1, "Sanity check we know how many methods we have.");
-
-            res.Should().AllBeEquivalentTo(
-                true,
-                $"all methods {string.Join(",", methods.Select(m => m.Name))} should be virtual"
+        var methods = typeof(AssemblyLoadContext)
+            .GetMethods()
+            .Where(m => m.IsConstructor == false)
+            .Where(m => objectMethods.Contains(m.Name) == false
             );
-        }
 
-        [Fact]
-        public void LoadFromStream_Stream_ShouldMimicSystem()
+        var res = methods
+            .Select(m => m.IsVirtual);
+
+        //  Act.
+        res.Count().Should().Be(1, "Sanity check we know how many methods we have.");
+
+        res.Should().AllBeEquivalentTo(
+            true,
+            $"all methods {string.Join(",", methods.Select(m => m.Name))} should be virtual"
+        );
+    }
+
+    [Fact]
+    public void LoadFromStream_Stream_ShouldMimicSystem()
+    {
+        var alc = new System.Runtime.Loader.AssemblyLoadContext("any");
+        var sut = new AssemblyLoadContext(alc);
+
+        var compiledAssembly = Compile(SourceCode);
+        using (var asm = new MemoryStream(compiledAssembly))
         {
-            var alc = new System.Runtime.Loader.AssemblyLoadContext("any");
-            var sut = new AssemblyLoadContext(alc);
+            //  Act.
+            var res = sut.LoadFromStream(asm);
 
-            var compiledAssembly = Compile(SourceCode);
-            using (var asm = new MemoryStream(compiledAssembly))
+            //  Assert.
+            res.FullName.Should().Be("Any.dll, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null");
+        }
+    }
+
+    private static byte[] Compile(string sourceCode)
+    {
+        using (var peStream = new MemoryStream())
+        {
+            var result = GenerateCode(sourceCode).Emit(peStream);
+            if (result.Success == false)
             {
-                //  Act.
-                var res = sut.LoadFromStream(asm);
-
-                //  Assert.
-                res.FullName.Should().Be("Any.dll, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null");
+                throw new Exception(string.Join(Environment.NewLine, result.Diagnostics.Select(d => d.ToString())));
             }
+            peStream.Seek(0, SeekOrigin.Begin);
+            return peStream.ToArray();
         }
+    }
 
-        private static byte[] Compile(string sourceCode)
-        {
-            using (var peStream = new MemoryStream())
-            {
-                var result = GenerateCode(sourceCode).Emit(peStream);
-                if (result.Success == false)
-                {
-                    throw new Exception(string.Join(Environment.NewLine, result.Diagnostics.Select(d => d.ToString())));
-                }
-                peStream.Seek(0, SeekOrigin.Begin);
-                return peStream.ToArray();
-            }
-        }
+    private static CSharpCompilation GenerateCode(string sourceCode)
+    {
+        var codeString = SourceText.From(sourceCode);
+        var options = CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp8);
 
-        private static CSharpCompilation GenerateCode(string sourceCode)
-        {
-            var codeString = SourceText.From(sourceCode);
-            var options = CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp8);
+        var parsedSyntaxTree = SyntaxFactory.ParseSyntaxTree(codeString, options);
 
-            var parsedSyntaxTree = SyntaxFactory.ParseSyntaxTree(codeString, options);
+        return CSharpCompilation.Create("Any.dll",
+            new[] { parsedSyntaxTree },
+            options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary,
+                optimizationLevel: OptimizationLevel.Debug,
+                assemblyIdentityComparer: DesktopAssemblyIdentityComparer.Default));
+    }
 
-            return CSharpCompilation.Create("Any.dll",
-                new[] { parsedSyntaxTree },
-                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary,
-                    optimizationLevel: OptimizationLevel.Debug,
-                    assemblyIdentityComparer: DesktopAssemblyIdentityComparer.Default));
-        }
-
-        private const string SourceCode = @"
+    private const string SourceCode = @"
 namespace AnyNameSpace
 {
 }";
-    }
 }
