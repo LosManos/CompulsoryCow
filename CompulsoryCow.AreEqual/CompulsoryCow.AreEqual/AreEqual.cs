1fd433f6e3679a49df57f420d0a0f892824a809d
Ola Fjelddahl <xomptolf@mio.se>
2022-12-07 23:59:54 +0100

    AreEqual:File scoped name space
    This commit contains no functional change.

--- CompulsoryCow.AreEqual/CompulsoryCow.AreEqual/AreEqual.cs
+++ CompulsoryCow.AreEqual/CompulsoryCow.AreEqual/AreEqual.cs
@@ -1,23 +1,23 @@
 ﻿using System;
 
-namespace CompulsoryCow.AreEqual
-{
-    public static class AreEqual
-	{
-        public enum Depth
-        {
-            Infinite = -1,
-            // 0 should not be used.
-            None = 1
-        }
+namespace CompulsoryCow.AreEqual;
 
-        /// <summary>This method compares every public property of an object and returns True if all of them are equal.
-        /// It returns False otherwise.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
+public static class AreEqual
+	{
+    public enum Depth
+    {
+        Infinite = -1,
+        // 0 should not be used.
+        None = 1
+    }
+
+    /// <summary>This method compares every public property of an object and returns True if all of them are equal.
+    /// It returns False otherwise.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
 		public static bool Public<T>(T a, T b)
 		{
 			var properties = Meta.GetPublicProperties(a);
@@ -35,69 +35,68 @@ namespace CompulsoryCow.AreEqual
 			return true;
 		}
 
-        /// <summary>This method compares two objects to a certain depth.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="depth">Infinite means turtles all the way down.
-        /// None is equal to 1 level deep and means just the first object and no check of any referenced object.
-        /// For any other depth, call the methdo with (AreEqual.Depth)#.</param>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
-        public static bool Public<T>(Depth depth, T a, T b)
-        {
-            var properties = Meta.GetPublicProperties(a);
+    /// <summary>This method compares two objects to a certain depth.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="depth">Infinite means turtles all the way down.
+    /// None is equal to 1 level deep and means just the first object and no check of any referenced object.
+    /// For any other depth, call the methdo with (AreEqual.Depth)#.</param>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static bool Public<T>(Depth depth, T a, T b)
+    {
+        var properties = Meta.GetPublicProperties(a);
 
-            foreach (var property in properties)
+        foreach (var property in properties)
+        {
+            if (IsSimple(property.PropertyType))
             {
-                if (IsSimple(property.PropertyType))
+                var propertyA = property.GetValue(a, null);
+                var propertyB = property.GetValue(b, null);
+
+                if (Equals(propertyA, propertyB) == false)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if ((int)depth >= 2 || depth == Depth.Infinite)
                 {
                     var propertyA = property.GetValue(a, null);
                     var propertyB = property.GetValue(b, null);
 
-                    if (Equals(propertyA, propertyB) == false)
+                    //  One is null and the other isn't means they are different.
+                    if(( propertyA == null && propertyB != null) ||
+                        (propertyA != null && propertyB == null))
                     {
                         return false;
                     }
-                }
-                else
-                {
-                    if ((int)depth >= 2 || depth == Depth.Infinite)
+                    if (propertyA != null && propertyB != null)
                     {
-                        var propertyA = property.GetValue(a, null);
-                        var propertyB = property.GetValue(b, null);
-
-                        //  One is null and the other isn't means they are different.
-                        if(( propertyA == null && propertyB != null) ||
-                            (propertyA != null && propertyB == null))
-                        {
-                            return false;
-                        }
-                        if (propertyA != null && propertyB != null)
-                        {
-                            var lessDepth = depth == Depth.Infinite ? Depth.Infinite : (Depth)((int)depth - 1);
-                            return Public(lessDepth, propertyA, propertyB);
-                        }
+                        var lessDepth = depth == Depth.Infinite ? Depth.Infinite : (Depth)((int)depth - 1);
+                        return Public(lessDepth, propertyA, propertyB);
                     }
                 }
             }
-
-            return true;
         }
 
-        private static bool IsSimple(Type type)
+        return true;
+    }
+
+    private static bool IsSimple(Type type)
+    {
+        // https://stackoverflow.com/questions/863881/how-do-i-tell-if-a-type-is-a-simple-type-i-e-holds-a-single-value
+
+        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
         {
-            // https://stackoverflow.com/questions/863881/how-do-i-tell-if-a-type-is-a-simple-type-i-e-holds-a-single-value
-
-            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
-            {
-                // nullable type, check if the nested type is simple.
-                return IsSimple(type.GetGenericArguments()[0]);
-            }
-            return type.IsPrimitive
-              || type.IsEnum
-              || type.Equals(typeof(string))
-              || type.Equals(typeof(decimal));
+            // nullable type, check if the nested type is simple.
+            return IsSimple(type.GetGenericArguments()[0]);
         }
+        return type.IsPrimitive
+          || type.IsEnum
+          || type.Equals(typeof(string))
+          || type.Equals(typeof(decimal));
     }
 }
--- CompulsoryCow.AreEqual/Tests/CompulsoryCow.AreEqual.Unit.Tests/PublicDepthTest.cs
+++ CompulsoryCow.AreEqual/Tests/CompulsoryCow.AreEqual.Unit.Tests/PublicDepthTest.cs
@@ -2,80 +2,79 @@
 using CompulsoryCow.AreEqual;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace AreEqualTest
+namespace AreEqualTest;
+
+[TestClass]
+public partial class PublicDepthTest : TestBase
 {
-    [TestClass]
-    public partial class PublicDepthTest : TestBase
+    [TestMethod]
+    public void PublicDepth_NoDepth_OnlyCompareImmediateObject()
     {
-        [TestMethod]
-        public void PublicDepth_NoDepth_OnlyCompareImmediateObject()
+        //  #   Arrange.
+        var equal = Dto.CreatePair(
+            _pr, 
+            new[] { Dto.Pair.Equal, Dto.Pair.Differs });
+        var notEqual = Dto.CreatePair(
+            _pr, 
+            new[] { Dto.Pair.Differs, Dto.Pair.Equal });
+        
+        //  #   Act.
+        var res1 = AreEqual.Public(AreEqual.Depth.None, equal.Item1, equal.Item2);
+        var res2 = AreEqual.Public(AreEqual.Depth.None, notEqual.Item1, notEqual.Item2);
+
+        //  #   Assert.
+        Assert.IsTrue(res1);
+        Assert.IsFalse(res2);
+    }
+
+    [TestMethod]
+    public void PublicDepth_SetDepth_OnlyCompareToThatDepth()
+    {
+        //  #   Arrange.
+        var equal = Dto.CreatePair(
+            _pr,
+            new[] { Dto.Pair.Equal, Dto.Pair.Equal, Dto.Pair.Differs });
+        var notEqual = Dto.CreatePair(
+            _pr,
+            new[] { Dto.Pair.Equal, Dto.Pair.Differs, Dto.Pair.Equal });
+
+        //  #   Act.
+        var res1 = AreEqual.Public((AreEqual.Depth) 2, equal.Item1, equal.Item2);
+        var res2 = AreEqual.Public((AreEqual.Depth) 2, notEqual.Item1, notEqual.Item2);
+
+        //  #   Assert.
+        Assert.IsTrue(res1);
+        Assert.IsFalse(res2);
+    }
+
+    [TestMethod]
+    public void PublicDepth_InfiniteDepth_StopForNothing()
+    {
+        //  #   Arrange.
+        var equal = Dto.CreatePair(
+            _pr,
+            CreateInfiniteEqualsList(Dto.Pair.Equal));
+        var notEqual = Dto.CreatePair(
+            _pr,
+            CreateInfiniteEqualsList(Dto.Pair.Differs));
+
+        //  #   Act.
+        var res1 = AreEqual.Public(AreEqual.Depth.Infinite, equal.Item1, equal.Item2);
+        var res2 = AreEqual.Public(AreEqual.Depth.Infinite, notEqual.Item1, notEqual.Item2);
+
+        //  #   Assert.
+        Assert.IsTrue(res1);
+        Assert.IsFalse(res2);
+    }
+
+    private static IEnumerable<Dto.Pair> CreateInfiniteEqualsList(Dto.Pair equal)
+    {
+        var res = new List<Dto.Pair>();
+        for(int i = 0; i <= 30; ++i)    //  Thirty is close to infinity.
         {
-            //  #   Arrange.
-            var equal = Dto.CreatePair(
-                _pr, 
-                new[] { Dto.Pair.Equal, Dto.Pair.Differs });
-            var notEqual = Dto.CreatePair(
-                _pr, 
-                new[] { Dto.Pair.Differs, Dto.Pair.Equal });
-            
-            //  #   Act.
-            var res1 = AreEqual.Public(AreEqual.Depth.None, equal.Item1, equal.Item2);
-            var res2 = AreEqual.Public(AreEqual.Depth.None, notEqual.Item1, notEqual.Item2);
-
-            //  #   Assert.
-            Assert.IsTrue(res1);
-            Assert.IsFalse(res2);
+            res.Add(Dto.Pair.Equal);
         }
-
-        [TestMethod]
-        public void PublicDepth_SetDepth_OnlyCompareToThatDepth()
-        {
-            //  #   Arrange.
-            var equal = Dto.CreatePair(
-                _pr,
-                new[] { Dto.Pair.Equal, Dto.Pair.Equal, Dto.Pair.Differs });
-            var notEqual = Dto.CreatePair(
-                _pr,
-                new[] { Dto.Pair.Equal, Dto.Pair.Differs, Dto.Pair.Equal });
-
-            //  #   Act.
-            var res1 = AreEqual.Public((AreEqual.Depth) 2, equal.Item1, equal.Item2);
-            var res2 = AreEqual.Public((AreEqual.Depth) 2, notEqual.Item1, notEqual.Item2);
-
-            //  #   Assert.
-            Assert.IsTrue(res1);
-            Assert.IsFalse(res2);
-        }
-
-        [TestMethod]
-        public void PublicDepth_InfiniteDepth_StopForNothing()
-        {
-            //  #   Arrange.
-            var equal = Dto.CreatePair(
-                _pr,
-                CreateInfiniteEqualsList(Dto.Pair.Equal));
-            var notEqual = Dto.CreatePair(
-                _pr,
-                CreateInfiniteEqualsList(Dto.Pair.Differs));
-
-            //  #   Act.
-            var res1 = AreEqual.Public(AreEqual.Depth.Infinite, equal.Item1, equal.Item2);
-            var res2 = AreEqual.Public(AreEqual.Depth.Infinite, notEqual.Item1, notEqual.Item2);
-
-            //  #   Assert.
-            Assert.IsTrue(res1);
-            Assert.IsFalse(res2);
-        }
-
-        private static IEnumerable<Dto.Pair> CreateInfiniteEqualsList(Dto.Pair equal)
-        {
-            var res = new List<Dto.Pair>();
-            for(int i = 0; i <= 30; ++i)    //  Thirty is close to infinity.
-            {
-                res.Add(Dto.Pair.Equal);
-            }
-            res.Add(equal);
-            return res;
-        }
+        res.Add(equal);
+        return res;
     }
 }
