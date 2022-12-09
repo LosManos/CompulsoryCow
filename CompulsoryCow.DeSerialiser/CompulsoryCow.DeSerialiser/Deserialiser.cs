71c1855d8392b4cda8057a59a6510f1532dcf3b1
Ola Fjelddahl <xomptolf@mio.se>
2022-12-09 22:42:59 +0100

    DeSerialiser:File scoped name space
    This commit contains no functional change.

--- CompulsoryCow.DeSerialiser/CompulsoryCow.DeSerialiser/Deserialiser.cs
+++ CompulsoryCow.DeSerialiser/CompulsoryCow.DeSerialiser/Deserialiser.cs
@@ -2,37 +2,36 @@
 using System.Xml;
 using System.Xml.Serialization;
 
-namespace CompulsoryCow
+namespace CompulsoryCow;
+
+/// <summary>This class is for deserialising objects.
+/// </summary>
+public static class Deserialiser
 {
-    /// <summary>This class is for deserialising objects.
+    /// <summary>This method deserialises an object from Xml.
+    /// If null is passed then null is returned.
+    ///  http://www.dotnetfunda.com/articles/article98.aspx
     /// </summary>
-    public static class Deserialiser
+    /// <param name="doc"></param>
+    /// <returns></returns>
+    public static T FromXml<T>(XmlDocument doc) where T:class 
     {
-        /// <summary>This method deserialises an object from Xml.
-        /// If null is passed then null is returned.
-        ///  http://www.dotnetfunda.com/articles/article98.aspx
-        /// </summary>
-        /// <param name="doc"></param>
-        /// <returns></returns>
-        public static T FromXml<T>(XmlDocument doc) where T:class 
+        if (null == doc)
         {
-            if (null == doc)
+            return null;
+        }
+        using (var stringreader = new StringReader(doc.InnerXml))
+        {
+            var serializer = new XmlSerializer(typeof (T));
+            XmlReader reader = new XmlTextReader(stringreader);
+            try
             {
-                return null;
+                var ret = (T) serializer.Deserialize(reader);
+                return ret;
             }
-            using (var stringreader = new StringReader(doc.InnerXml))
+            finally
             {
-                var serializer = new XmlSerializer(typeof (T));
-                XmlReader reader = new XmlTextReader(stringreader);
-                try
-                {
-                    var ret = (T) serializer.Deserialize(reader);
-                    return ret;
-                }
-                finally
-                {
-                    reader.Close();
-                }
+                reader.Close();
             }
         }
     }
--- CompulsoryCow.DeSerialiser/CompulsoryCow.DeSerialiser/Deserializer.cs
+++ CompulsoryCow.DeSerialiser/CompulsoryCow.DeSerialiser/Deserializer.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Xml;
 
-namespace CompulsoryCow
+namespace CompulsoryCow;
+
+[Obsolete("This class has been renamed to Deserialiser.")]
+public static class Deserializer
 {
-    [Obsolete("This class has been renamed to Deserialiser.")]
-    public static class Deserializer
+    [Obsolete("This method has been moved to Serialiser class.")]
+    public static T FromXml<T>(XmlDocument doc) where T : class
     {
-        [Obsolete("This method has been moved to Serialiser class.")]
-        public static T FromXml<T>(XmlDocument doc) where T : class
-        {
-            throw new NotImplementedException("This method has been moved to Serialiser class.");
-        }
+        throw new NotImplementedException("This method has been moved to Serialiser class.");
     }
 }
