944bfd1f41ddf21371b0900641b263e8b2bcb795
Ola Fjelddahl <xomptolf@mio.se>
2023-01-29 22:36:36 +0100

    Meta:Method that returns all classes that has a certain Attribute
    This commit is part of #81.

---
+++ CompulsoryCow.Meta/Tests/CompulsoryCow.Meta.Unit.Tests/GetAttributeTests.cs
@@ -0,0 +1,27 @@
+﻿using CompulsoryCow;
+using FluentAssertions;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace MetaTest;
+
+public class GetAttributeTests
+{
+    [Fact]
+    public void GetClassesWithAttribute_Should_return_classes_with_a_certain_attribute()
+    {
+        var res = Meta.GetClassesWithAttribute<MyAttribute>(AppDomain.CurrentDomain);
+
+        res.Single().FullName.Should().Be(typeof(MyClass).FullName);
+    }
+
+    public class MyAttribute : Attribute
+    {
+    }
+
+    [MyAttribute]
+    public class MyClass
+    {
+    }
+}
