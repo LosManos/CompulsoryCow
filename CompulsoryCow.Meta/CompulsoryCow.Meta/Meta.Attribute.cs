f93a78d8b3a783d839030be3384d710a83b3f4f4
Ola Fjelddahl <xomptolf@mio.se>
2023-01-29 22:36:36 +0100

    Meta:Method that returns all classes that has a certain Attribute
    This commit is part of #81.

---
+++ CompulsoryCow.Meta/CompulsoryCow.Meta/Meta.Attribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace CompulsoryCow;
+
+public partial class Meta
+{
+    /// <summary>This method returns the types of all classes that are decorated iwth the attribute.
+    /// I nothing is found an empty enumerable is returned.
+    /// Call it like so:
+    /// 
+    /// var myRunningDomain = AppDomain.CurrentDomain)
+    /// IEnumerable&lt;Type&gt; result = Meta.GetClassesWithAttribute&lt;MyAttribute&gt(myRunningDomain)
+    /// 
+    /// and all loaded(?) classes decorated with `MyAttribute` are returned.
+    /// </summary>
+    /// <typeparam name="TAttribute"></typeparam>
+    /// <param name="appDomain"></param>
+    /// <returns></returns>
+    public static IEnumerable<Type> GetClassesWithAttribute<TAttribute>(AppDomain appDomain) where TAttribute : Attribute
+    {
+        return appDomain.GetAssemblies()
+            .SelectMany(a => a.GetTypes())
+            .Where(t => t.IsDefined(typeof(TAttribute)));
+    }
+}
