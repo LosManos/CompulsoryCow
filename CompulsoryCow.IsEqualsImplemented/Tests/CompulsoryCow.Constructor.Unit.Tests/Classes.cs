f6e495b47bfb64fbf74c8d20752ad857e244ed01
Ola Fjelddahl <xomptolf@mio.se>
2023-02-17 19:20:35 +0100

    IsEqualsImplemented:[0.5.0]:Add methods Constructor.IsDefaultImplemented
    This commit adds methods Constructor.IsDefaultImplemented that returns whether a class contains a default constructor.
    
    This commit solves #81.

---
+++ CompulsoryCow.IsEqualsImplemented/Tests/CompulsoryCow.Constructor.Unit.Tests/Classes.cs
@@ -0,0 +1,25 @@
+﻿namespace CompulsoryCow.Constructor.Unit.Tests;
+
+public class ImplicitDefaultConstructorAttribute : Attribute{}
+
+[ImplicitDefaultConstructor]
+public class MyImplicitClass{}
+
+public class NoDefaultConstructorAttribute : Attribute{}
+
+[ExplicitDefaultConstructor]
+public class MyExplicitClass
+{
+    public MyExplicitClass() { }
+}
+
+public class ExplicitDefaultConstructorAttribute : Attribute{}
+
+[NoDefaultConstructor]
+public class MyNoDefaultClass
+{
+    public MyNoDefaultClass(int _)
+    { }
+}
+
+public class AttributeNoOneHasImplemented : Attribute { }
