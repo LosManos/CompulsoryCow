79d5d1da8f2969e9c413680379e789a932fb4c8c
ola.fjelddahl@diespammers.gmail.com <unknown>
2023-05-28 16:50:32 +0200

    IsImplemented:New package with IsEqualsImplementeed
    This commit creates the new nuget IsImplemented package.
    It is a copy of the IsEqualsImplemented package
    which has been deprecated with a reference to this new package.
    
    This commit solves #81.

---
+++ CompulsoryCow.IsImplemented/CompulsoryCow.IsImplemented/TypeNotRecognisedException.cs
@@ -0,0 +1,12 @@
+namespace CompulsoryCow.IsImplemented;
+
+[Serializable]
+public class TypeNotRecognisedException : Exception
+{
+    public TypeNotRecognisedException() { }
+    public TypeNotRecognisedException(string message) : base(message) { }
+    public TypeNotRecognisedException(string message, Exception inner) : base(message, inner) { }
+    protected TypeNotRecognisedException(
+        System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
